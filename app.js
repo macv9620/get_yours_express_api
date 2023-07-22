e054e57c79861fc3cec9c19fc96ba693d53eddc2
Mateo Vasco <macv9620@utp.edu.co>
2023-07-21 21:56:28 -0500

    Se ajusta respuesta para get items

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors')
+
+
 
 require('dotenv').config()
 
@@ -13,6 +16,7 @@ const statusRouter = require('./routes/status');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/items.js
+++ routes/items.js
@@ -22,8 +22,10 @@ router.get('/', async function(req, res, next) {
     })
     const detail = items.map((item)=> {
   
-      item['brand'] = item['brand_product_brandTobrand'];
+      item['brand_detail'] = item['brand_product_brandTobrand'];
       delete item['brand_product_brandTobrand'];
+
+      item['brand'] = item.brand_detail.name;
   
       item['product_name'] = item['name'];
       delete item['name'];
@@ -31,8 +33,11 @@ router.get('/', async function(req, res, next) {
       item['status'] = item['status_product_statusTostatus'];
       delete item['status_product_statusTostatus'];
   
-      item["category"] = item["category_product_categoryTocategory"]
+      item["category_detail"] = item["category_product_categoryTocategory"]
       delete item["category_product_categoryTocategory"]
+
+      item['category'] = item.category_detail.name;
+
   
       return item;
     })
@@ -42,5 +47,26 @@ router.get('/', async function(req, res, next) {
     res.status(500).send({"message": err.message})
   }
 });
+
+/* POST create items */
+router.post('/', async function(req, res, next) {
+  try{
+    const item = await prisma.product.create({
+      data: {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        brand: req.body.brand,
+        sku: req.body.sku,
+        image: req.body.image,
+        status: req.body.status
+      }
+    })
+    res.status(200).send(item)
+  } catch(err){
+    res.status(500).send({"message": err.message})
+  }
+  });
 
 module.exports = router;
